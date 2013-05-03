1db4bf0b1cb198b40b45dcd9535227fa52811054
Andreas Renberg (IQAndreas) <iq_andreas@hotmail.com>
2013-05-03 12:55:09 +0200

    Update submodule
    The code now searches in the new `blacklists` folder for the blacklists.
    
    Also throws errors if a blacklist cannot be found, or if a blacklist
    contains an invalid regular expression.

--- spamfilter.php
+++ spamfilter.php
@@ -1,7 +1,7 @@
 <?php
 
 // spamfilter.php -- Filter through text, searching for spam
-// Copyright (C) 2012 Andreas Renberg <iq_andreas@hotmail.com>
+// Copyright (C) 2013 Andreas Renberg <iq_andreas@hotmail.com>
 //
 //  This program is free software; you can redistribute it and/or modify it
 //  under the terms of the GNU General Public License version 3, as
@@ -15,31 +15,72 @@
 //  You should have received a copy of the GNU General Public License along
 //  with this program; if not, see <http://www.gnu.org/licences/>
 
+define(SPAM_BLACKLISTS_DIR, dirname(__FILE__) . DIRECTORY_SEPARATOR . 'blacklists' . DIRECTORY_SEPARATOR );
+define(ALL_BLACKLISTS, SPAM_BLACKLISTS_DIR . 'blacklist-*.txt');
 
-function spam_check_text($text, $blacklist = 'spam_blacklist_keywords.txt')
+function spam_check_text($text, $blacklist = ALL_BLACKLISTS)
 {
-	return regex_match_from_file($text, $blacklist);
-}
-
-function spam_check_url($url, $blacklist = 'spam_blacklist_urls.txt')
-{
-	// I'm lazy. Just do the same here
-	return regex_match_from_file($url, $blacklist);
-}
-
-function regex_match_from_file($text, $blacklist)
-{
-	$keywords = file($blacklist);
-
-	foreach($keywords as $keyword) 
+	if ($blacklist == ALL_BLACKLISTS)
 	{
-		// Remove comments and whitespace before and after a keyword
-		$keyword = preg_replace('/(^\s+|\s+$|\s*#.*$)/i', "", $keyword);
-		
-		if (!empty($keyword) && preg_match("/$keyword/i", $text))
+		$blacklists = glob(ALL_BLACKLISTS);
+		foreach ($blacklists as $blacklist_filename)
 		{
-			// Spam found
-			return $keyword;
+			$match = regex_match_from_blacklist($text, $blacklist_filename);
+			if ($match) return $match;
+		}
+	}
+	else
+	{
+		return regex_match_from_blacklist($text, $blacklist);
+	}
+}
+
+function spam_check_url($url, $blacklist = ALL_BLACKLISTS)
+{
+	// TODO! Just treat the url as plain text for now.
+	return spam_check_text($url, $blacklist);
+}
+
+function regex_match_from_blacklist($text, $blacklist)
+{
+	if (!file_exists($blacklist))
+	{
+		// Check to see if they supplied a relative path instead of an absolute one.
+		$blacklist_absolute = SPAM_BLACKLISTS_DIR . $blacklist;
+		if (file_exists($blacklist_absolute))
+		{
+			$blacklist = $blacklist_absolute;
+		}
+		else
+		{
+			// Is this the proper way to throw errors in PHP?
+			trigger_error("[spamfilter.php::regex_match_from_blacklist()] Error: Cannot find blacklist with name `$blacklist`.");
+			return false;
+		}
+	}
+	
+	$keywords = file($blacklist);
+	$current_line = 0;
+	$regex_match = array();
+	
+	foreach($keywords as $regex) 
+	{
+		$current_line++;
+		
+		// Remove comments and whitespace before and after a keyword
+		$regex = preg_replace('/(^\s+|\s+$|\s*#.*$)/i', "", $regex);
+		if (emtpy($regex)) continue;
+		
+		$match = @preg_match("/$regex/i", $text, $regex_match);
+		if ($match)
+		{
+			// Spam found. Return the text that was matched
+			return $regex_match[0];
+		}
+		else if ($match === false)
+		{
+			trigger_error("[spamfilter.php::regex_match_from_blacklist()] Error: Invalid regular expression in `$blacklist` line $current_line.");
+			continue;	
 		}
 	}
 	
